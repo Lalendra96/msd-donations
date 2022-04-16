da02b9ff767a89bc4749fe1444dc99e0b5672016
Lalendra96 <32574047+Lalendra96@users.noreply.github.com>
2022-04-16 20:33:28 +0530

    Revert "Merge branch 'main' into dev"
    This reverts commit 5b50587ca2583d13cfb0c3692bd46beeec4d635f, reversing
    changes made to 01f666d7ca73daff40876ef484f08def9854cc2a.

---
+++ resources/views/emails/mail.blade.php
@@ -0,0 +1,5 @@
+Hello <strong>{{ $name }}</strong>,
+<p>Donated Amount of <strong>{{$body}}</strong> Received.</p>
+<p>As a Country, We highly Appreciate and greatful for Your donation.</p>
+<p>Thank You,</p>
+<p>State Ministry of Production, Supply and Regulation of Pharmaceuticals</p>
